73a19e362fdc3449c3e65661414e2e36fd04fabf
YisiHuang <20095257@mail.wit.ie>
2023-09-14 01:36:40 +0800

    Dynamic properties.

--- fundamentals.js
+++ fundamentals.js
@@ -14,3 +14,13 @@ console.log(
     aCar.registration.county + "-" +
     aCar.registration.number
 );
+
+aCar.mileage = 10000
+aCar.colour = {
+  exterior: "red",
+  interior: { texture: "leather", shade: "cream"}
+}
+console.log( "It is a " 
+            + aCar.colour.exterior + " car, " 
+            + aCar.mileage + " mileage, with " 
+            + aCar.colour.interior.texture + " interior.")
